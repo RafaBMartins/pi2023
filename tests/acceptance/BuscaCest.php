3ceaa6646fb49b6380aaeed47de380c552a2aee4
caiofraga123 <92186508+caiofraga123@users.noreply.github.com>
2023-06-28 09:18:04 -0300

    Update BuscaCest.php

--- tests/acceptance/BuscaCest.php
+++ tests/acceptance/BuscaCest.php
@@ -15,7 +15,7 @@ class BuscaCest
     public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
     {
         $I->amOnPage('/pi2023/web/html/index.html');
-        $I->fillField('email-input', 'rafaelmartinscouto@gmail.com');
+        $I->fillField('email', 'rafaelmartinscouto@example.com');
         $I->fillField('senha', 'Gabriel123gabriel!');
         $I->click('ENTRAR');
         $I->seeCurrentURLEquals('/web/html/home.html');
