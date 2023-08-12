f16e553934f6a29b8928a90beab75b74167dca73
Rafael Barbosa Martins <rafaelmartinscouto@gmail.com>
2023-08-12 16:55:46 -0300

    12/08

--- web/headerFooter/header.js
+++ web/headerFooter/header.js
@@ -1,11 +1,9 @@
-/*adiciona um evento de clique ao botao de configuracoes, faz com que o menu de configuracoes abra ao ser cliacado, e feche caso seja clicado novamente ou o clique aconteca fora dele*/
-document.querySelector("body").addEventListener('click', (e) => {
-   if (document.getElementsByClassName("fa-gear")[0].contains(e.target)) {
-      document.querySelector(".containerOpcoes").style.display = document.querySelector(".containerOpcoes").style.display === "flex" ? "none" : "flex";
-      document.getElementsByClassName("fa-gear")[0].style.display = "none";
-   }
-   else if (document.getElementById("closeConfig").contains(e.target) || (!document.querySelector(".containerOpcoes").contains(e.target) && document.querySelector(".containerOpcoes").style.display != "none")) {
-      document.querySelector(".containerOpcoes").style.display = "none";
-      document.getElementsByClassName("fa-gear")[0].style.display = "block";
-   }
-})
+function mobileMenu() {
+    var options = document.getElementById("header_options");
+    if (options.style.display === "flex") {
+      options.style.display = "none";
+    } else {
+      options.style.display = "flex";
+    }
+  }
+  
