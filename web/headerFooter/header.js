f14d7f69598a8e02f4b4267369a52ed6686fd1c7
Rafael Barbosa Martins <rafaelmartinscouto@gmail.com>
2023-08-13 14:57:15 -0300

    header done and commented 13/08

--- web/headerFooter/header.js
+++ web/headerFooter/header.js
@@ -1,3 +1,4 @@
+/*display on or off when clicking on the mobile menu*/
 function mobileMenu() {
     var options = document.getElementById("header_options");
     if (options.style.display === "flex") {
