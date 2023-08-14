b7afaea46ea4ab79bab54259620ec897308615a1
Rafael Barbosa Martins <rafaelmartinscouto@gmail.com>
2023-08-13 21:58:24 -0300

    usbwebserver create and php header and footer include on pages

--- web/html/header.php
+++ web/html/header.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/fontsAndColors.css">
-    <script src="header.js"></script>
+    <script src="../script/header.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>Header</title>
@@ -30,14 +30,14 @@
 
             <!--all options-->
             <div id="header_options">
-                <a href="../html/mapa.html" class="header-link">Mapa</a>
+                <a href="mapa.php" class="header-link">Mapa</a>
                 <a href="" class="header-link">Sobre nós</a>
-                <a href="../html/registrar.html" class="header-btnRegister">CRIAR CONTA</a>
-                <a href="../html/registrar.html" class="header-link-mobile">CRIAR CONTA</a><!--for mobile-->
-                <a href="../html/index.html" class="header-btnLogin">ENTRAR</a>
-                <a href="../html/index.html" class="header-link-mobile">ENTRAR</a><!--for mobile-->
-                <a href="../html/index.html" class="header-link-mobile">CONTA</a><!--for mobile-->
-                <a href="../html/perfilusuario.html" class="header-iconLink">
+                <a href="registrar.html" class="header-btnRegister">CRIAR CONTA</a>
+                <a href="registrar.html" class="header-link-mobile">CRIAR CONTA</a><!--for mobile-->
+                <a href="login.html" class="header-btnLogin">ENTRAR</a>
+                <a href="login.html" class="header-link-mobile">ENTRAR</a><!--for mobile-->
+                <a href="perfilusuario.php" class="header-link-mobile">CONTA</a><!--for mobile-->
+                <a href="perfilusuario.php" class="header-iconLink">
                     <i class="fa-solid fa-address-card"></i>
                 </a>
             </div>
