8e814db0bf5947116a295122a6f39deb96fa4373
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-08-15 09:02:44 -0300

    15/08

---
+++ web/script/cadastro.js
@@ -0,0 +1,7 @@
+document.getElementById("selectImg").addEventListener('click', (e) => {
+    document.getElementById("imgPerfil").click();
+})
+
+document.getElementById("selectImgAval").addEventListener('click', (e) => {
+    document.getElementById("imgAval").click();
+})
