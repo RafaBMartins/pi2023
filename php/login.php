6dbe3a31ac0f87b9b6e05ba2d1b46f261edb4b8e
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-09-19 08:44:28 -0300

    19/09

--- php/login.php
+++ php/login.php
@@ -1,17 +1,28 @@
-<?php 
+<?php
+$validos = array();
 $email = $_POST["email"];
+var_dump($email);
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo("$email é um email válido");
+    echo("$email e um email valido");
+    array_push($validos, true);
   } else {
-    echo("$email não é um email válido");
+    echo("$email nao e um email valido");
   }
 
   $password = $_POST["password"];
   $regex = "/^(.{8,16})$/";
   if (preg_match($regex,$password)) {
-    echo("$password é uma senha válida");
+    echo("$password e uma senha valida");
+    array_push($validos, true);
   } else {
-    echo("$password não é uma senha válida");
+    echo("$password não e uma senha valida");
+  }
+
+  if(sizeof($validos) == 2){
+    session_start();
+    $_SESSION["email"] = $email;
+    $_SESSION["senha"] = $password;
+    header('location: C:\Users\20211tiimi0168\Desktop\server\root\index.php');
   }
 ?>
