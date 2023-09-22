45aa1c41c876db5d3de7334c7e321f8630d64d58
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-09-22 12:53:29 -0300

    22/09

--- php/loginRegister.php
+++ php/loginRegister.php
@@ -1,62 +1,75 @@
 <?php 
 function sanitizeEmail($email) {
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL)
-    return $email
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    return $email;
 }
 
 function sanitizeUserName($userName) {
-    $userName = filter_var($userName, FILTER_SANITIZE_SPECIAL_CHARS)
-    return $userName
+    $userName = filter_var($userName, FILTER_SANITIZE_SPECIAL_CHARS);
+    return $userName;
 }
 
 function validatePassword($password) {
-    return preg_match("/^(.{8,16})$/",$password)
+    return preg_match("/^(.{8,16})$/",$password);
 }
 
 function comparePassword($password1, $password2) {
-    return $password1 == $password2
+    return $password1 == $password2;
 }
 
 function validateUserName($userName) {
-    return preg_match("/^.{1,20}$/",$userName)
+    return preg_match("/^.{1,20}$/",$userName);
 }
 
 function validateEmail($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL)
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
-$formType = $_POST["type"]
-$usersArray = array()
+function findUser($email){
+    $banco = fopen("bancoUsuarios.txt", "r");
+    while(($linha=fgets($banco)) != false){
+        $dados = explode(";", $linha);
+        if($dados{0} == $email){
+            return substr($dados{1}, 0, -1);
+        }
+    }
+}
+
+$formType = $_POST["type"];
+
 
 if($formType == "register") {
-    $userName = $_POST["userName"]
-    $email = $_POST["email"]
-    $password = $_POST["password"]
+    $userName = $_POST["userName"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-    $email = sanitizeEmail($email)
-    $userName = sanitizeUserName($userName)
+    $email = sanitizeEmail($email);
+    $userName = sanitizeUserName($userName);
 
-    $validationConfirmsArray = array("password" => false, "userName" => false, "email" => false)
-    $validationConfirmsArray["password"] = validatePassword($password) and comparePassword($password, $_POST["passwordAgain"])
-    $validationConfirmsArray["userName"] = validateUserName($userName)
-    $validationConfirmsArray["email"] = validateEmail($email)
+    $validationConfirmsArray = array("password" => false, "userName" => false, "email" => false);
+    $validationConfirmsArray["password"] = validatePassword($password) and comparePassword($password, $_POST["passwordAgain"]);
+    $validationConfirmsArray["userName"] = validateUserName($userName);
+    $validationConfirmsArray["email"] = validateEmail($email);
 
     if($validationConfirmsArray["password"] and $validationConfirmsArray["userName"] and $validationConfirmsArray["email"]) {
-        $usersArray[$email] = $password
-        header("location: http://localhost/pi2023/login.html")
+        $banco = fopen("bancoUsuarios.txt", "a");
+        $txt = $email . ";" . $password . "\n";
+        fwrite($banco, $txt);
+        fclose($banco);
+        header("location: http://localhost:8080/pi2023/login.html");
     }
 }
 
 if($formType == "login") {
-    $email = $_POST["email"]
-    $password = $_POST["password"]
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-    $email = sanitizeEmail($email)
+    $email = sanitizeEmail($email);
 
-    if($usersArray[$email] == $password) {
-        session_start()
-        $_SESSION[$email]
-        header("location: http://localhost/pi2023/index.php")
+    if(findUser($email) == $password) {
+        session_start();
+        $_SESSION["email"] = $email;
+        header("location: http://localhost:8080/pi2023/index.php");
     }
 }
 ?>
