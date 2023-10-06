6d473d9aa2297b3b082237717cf0713250059aca
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-10-06 11:10:38 -0300

    06/10

--- php/pdoTeste.php
+++ php/pdoTeste.php
@@ -1,5 +1,5 @@
 <?php
-
+try{
 $db_host="host = silly.db.elephantsql.com;";
 $db_port="port = 5432;";
 $db_user="blaiyjfo";
@@ -10,7 +10,7 @@ $db_name="dbname = PI2023;";
 
 //$db= new PDO('mysql:host=localhost;dbname='. $db_name . ';charset=utf8', $db_user,$db_password);
 
-$db= new PDO('pgsql:' . $db_host . $db_port . $db_name . ';charset=utf8', $db_user, $db_password);
+$db= new PDO('pgsql:' . $db_host . $db_port . $db_name . $db_user . $db_password);
 
 if($db){
     echo "Sucesso";
@@ -20,8 +20,24 @@ if($db){
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
 $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,true);
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+
+catch(Exception $e){
+    echo $e;
+}
 
 //define('APP_NAME', 'PHP REST API TUTORIAL - PROFA MARTA');
 
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
