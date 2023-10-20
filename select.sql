341dfb5a5f93b02b28f2ee7c68cdeebf0c332b06
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-10-20 11:12:25 -0300

    20/10

--- select.sql
+++ select.sql
@@ -1,12 +1,14 @@
-SELECT nome,
-		tipo_estabelecimento.tipo_estabelecimento, 
-		selo.selo, 
-		foto_banner,
-		nota,
-		2 * ATAN2(SQRT(POWER(SIN(((PI()*latitude)/180 - (PI()*user_latitude)/180)/2), 2) + COS((PI()*user_latitude)/180) * COS((PI()*latitude)/180) * POWER(SIN(((PI()*longitude)/180 - (PI()*longitude)/180)/2), 2)), 1 - SQRT(POWER(SIN(((PI()*latitude)/180 - (PI()*user_latitude)/180)/2), 2) + COS((PI()*user_latitude)/180) * COS((PI()*latitude)/180) * POWER(SIN(((PI()*longitude)/180 - (PI()*longitude)/180)/2), 2))) * 6371 as distancia
-FROM estabelecimento
-INNER JOIN tipo_estabelecimento
-ON tipo_estabelecimento.tipo_estabelecimento_pk = estabelecimento.fk_tipo_estabelecimento_tipo_estabelecimento_pk
-INNER JOIN selo
-ON selo.selo_pk = estabelecimento.fk_selo_selo_pk
+SELECT 4 * ATAN2(SQRT(POWER(SIN(((PI()* -20.15937/180 - (PI() * -19.585)/180)/2)), 2) + COS((PI() * -39.784)/180) * COS((PI()* -20.15937/180)) * POWER(SIN(((PI()*-40.25339)/180 - (PI()*-40.25339)/180)/2), 2)), 1 - SQRT(POWER(SIN(((PI()* -20.15937)/180 - (PI() * -19.585)/180)/2), 2) + COS((PI() * -39.784)/180) * COS((PI()* -20.15937)/180) * POWER(SIN(((PI()*-40.25339)/180 - (PI()* -40.25339)/180)/2), 2))) * 6371 AS distancia
+FROM ESTABELECIMENTO
 ORDER BY distancia DESC;
+
+SELECT  6372.795477598 * acos(sin(-20.15937) * sin (-19.585*Pi()/180) + cos (-20.15937*PI()) * cos (-19.585*Pi()/180) * cos ( -40.25339 - -39.784))
+FROM ESTABELECIMENTO
+ORDER BY distancia DESC;
+
+select	@dLat = (-20.15937 - -19.585)*PI()/180, 
+		@dLong = (-40.25339 - -39.784)*PI()/180,
+		@a = sin(dLat/2) * sin(dLat/2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon/2) * sin(dLon/2),
+		@c = 2 * ATAN2(SQRT(a), SQRT(1-a)),
+		c * 6372.795 as d;
+		
