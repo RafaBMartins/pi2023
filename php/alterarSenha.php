a0ea47ad70f1856af9e9dec1a09473ce87a6b205
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-11-23 17:39:05 -0300

    23/11

--- php/alterarSenha.php
+++ php/alterarSenha.php
@@ -4,12 +4,18 @@
     if(isset($_SESSION["email"]) && isset($_POST["senhaAtual"]) && isset($_POST["newPassword"]) && isset($_POST["confirmNewPassword"])){
         $resposta = array();
         $senhaAtual = $_POST["senhaAtual"];
+        var_dump($_POST["senhaAtual"]);
         $email = $_SESSION["email"];
-        $consultaToken = $db->prepare("SELECT senha FROM usuario where email = '$email'");
-        var_dump($consultaToken);
+        $consultaToken = $db->prepare("SELECT senha FROM usuario WHERE email = '$email'");
         $consultaToken->execute();
         $linha = $consultaToken->fetch(PDO::FETCH_ASSOC);
         var_dump($linha);
+        if(password_verify($senhaAtual, $linha["senha"])){
+            echo "teste";
+        }
+        else{
+            echo "não";
+        }
         
 
     }
@@ -17,6 +23,6 @@
         $resposta["sucesso"] = 0;
         $resposta["erro"] = "faltam parametros";
     }
-    header("location: http://localhost:8080/pi2023/pusu.php");
-    die();
+    //header("location: http://localhost:8080/pi2023/pusu.php");
+    //die();
 ?>
