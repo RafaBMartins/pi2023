70397358d21cef4057b9901569800b37e13060a5
RafaBMartins <rafaelmartinscouto@gmail.com>
2023-12-01 12:00:04 -0300

    01/12

---
+++ script/estrelas.js
@@ -0,0 +1,7 @@
+estrelas = [...document.getElementsByClassName("estrelaAvaliacao")];
+estrelas.forEach(estrela => {
+    estrela.addEventListener('mouseover', (e) => {
+        console.log(e.srcElement.id)
+        for(let i )
+    })
+});
